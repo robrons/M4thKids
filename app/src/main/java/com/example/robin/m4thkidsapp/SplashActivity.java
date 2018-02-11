702205b109f36feacbebfa67209945e269137acc
Robin Ronson <robin_ronson@yahoo.com>
2018-02-11 10:57:07 -0600

    Added inital Splash

---
+++ app/src/main/java/com/example/robin/m4thkidsapp/SplashActivity.java
@@ -0,0 +1,21 @@
+
+/**
+ * Created by robin on 2/11/2018.
+ */
+package com.example.robin.m4thkidsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
