de24c62818f70a644c7a2c186648b5c45e5fb4c4
trhuckabe <victoria.huckabee@ttu.edu>
2018-02-13 06:41:08 -0600

    Code for Topics Menu

--- app/src/main/java/com/example/robin/m4thkidsapp/MainActivity.java
+++ app/src/main/java/com/example/robin/m4thkidsapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.robin.m4thkidsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.robin.m4thkidsapp";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void filler_fun(View view) {
+        Intent intent = new Intent(this, topic_menu.class);
+        startActivity(intent);
     }
 }
