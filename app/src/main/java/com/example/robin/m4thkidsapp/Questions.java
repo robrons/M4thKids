8e26032202345b61e8687314dbdbab66ff383197
trhuckabe <victoria.huckabee@ttu.edu>
2018-04-05 19:28:37 -0500

    small changes

--- app/src/main/java/com/example/robin/m4thkidsapp/Questions.java
+++ app/src/main/java/com/example/robin/m4thkidsapp/Questions.java
@@ -12,6 +12,10 @@ public class Questions extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
         setContentView(R.layout.activity_questions);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
