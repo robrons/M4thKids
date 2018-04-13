6c15d1b3922c3d796e3bfc7d7c71478c36820256
Abraham Quijada <abe.quijada@gmail.com>
2018-04-13 15:57:39 -0500

    Mp3 File

--- app/src/main/java/com/example/robin/m4thkidsapp/MainActivity.java
+++ app/src/main/java/com/example/robin/m4thkidsapp/MainActivity.java
@@ -3,33 +3,57 @@ package com.example.robin.m4thkidsapp;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Dialog myDialog;
+    MediaPlayer mySong;
+    boolean music = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDialog = new Dialog(this);
+        mySong = MediaPlayer.create(MainActivity.this,R.raw.buddy);
+        mySong.isLooping();
+        mySong.start();
     }
     public void ShowPopup(View v) {
         TextView txtclose;
-        Button btnFollow;
+        TextView txtmusic;
         myDialog.setContentView(R.layout.custompopup);
-        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
-        txtclose.setText("M");
-        //btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose = myDialog.findViewById(R.id.resume);
+        txtmusic = myDialog.findViewById(R.id.music);
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myDialog.dismiss();
             }
         });
+
+
+        txtmusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(music){
+                    myDialog.findViewById(R.id.linearLayout6).setBackgroundColor(Color.parseColor("#85e085"));
+                    ((TextView)myDialog.findViewById(R.id.ll6text)).setText("On");
+                    mySong.start();
+                    music = false;
+                }
+                else{
+                    myDialog.findViewById(R.id.linearLayout6).setBackgroundColor(Color.parseColor("#ff9999"));
+                    ((TextView)myDialog.findViewById(R.id.ll6text)).setText("Off");
+                    mySong.pause();
+                    music = true;
+                }
+            }
+        });
+
         myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         myDialog.show();
     }
